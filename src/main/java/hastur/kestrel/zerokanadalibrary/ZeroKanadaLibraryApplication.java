d3830dcdefb388afa53da406d42887b287b811b8
GirTabAleph <nineteencycles@gmail.com>
2026-07-12 15:08:12 -0600

    Initial commit

---
+++ src/main/java/hastur/kestrel/zerokanadalibrary/ZeroKanadaLibraryApplication.java
@@ -0,0 +1,13 @@
+package hastur.kestrel.zerokanadalibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroKanadaLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroKanadaLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hastur/kestrel/zerokanadalibrary/ZeroKanadaLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package hastur.kestrel.zerokanadalibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroKanadaLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
