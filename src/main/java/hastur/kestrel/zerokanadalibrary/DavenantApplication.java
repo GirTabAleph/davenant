6b5934c86f67bc1365bc2be5f7127482682f415c
GirTabAleph <nineteencycles@gmail.com>
2026-08-17 00:48:21 -0600

    Maven and SpringBootApplication changes to reflect new project name

--- src/main/java/hastur/kestrel/zerokanadalibrary/DavenantApplication.java
+++ src/main/java/hastur/kestrel/zerokanadalibrary/DavenantApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZeroKanadaLibraryApplication {
+public class DavenantApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZeroKanadaLibraryApplication.class, args);
+        SpringApplication.run(DavenantApplication.class, args);
     }
 
 }
