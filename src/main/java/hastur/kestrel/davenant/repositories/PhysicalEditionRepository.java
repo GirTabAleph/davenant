6850408c381add4a5fde29f1d670daadabd09b1b
GirTabAleph <nineteencycles@gmail.com>
2026-08-20 19:05:46 -0600

    Extended PhysicalEditionRepository

--- src/main/java/hastur/kestrel/davenant/repositories/PhysicalEditionRepository.java
+++ src/main/java/hastur/kestrel/davenant/repositories/PhysicalEditionRepository.java
@@ -2,11 +2,16 @@ package hastur.kestrel.davenant.repositories;
 
 import hastur.kestrel.davenant.models.PhysicalEditionModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PhysicalEditionRepository extends JpaRepository<PhysicalEditionModel, Integer> {
 
+    public List<PhysicalEditionModel> findAllByIsbn(String isbn);
 
+    public List<PhysicalEditionModel> findAllByEditionName(String editionName);
 
 }
