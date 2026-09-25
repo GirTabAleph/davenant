7da773becd129b17cac45b958d6693c574cba236
GirTabAleph <nineteencycles@gmail.com>
2026-09-25 15:03:49 -0600

    wrap method with Optional

--- src/main/java/hastur/kestrel/davenant/repositories/PublisherRepository.java
+++ src/main/java/hastur/kestrel/davenant/repositories/PublisherRepository.java
@@ -4,9 +4,11 @@ import hastur.kestrel.davenant.models.PublisherModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PublisherRepository extends JpaRepository<PublisherModel, Integer> {
 
-    public PublisherModel findPublisherModelByName(String publisherName);
+    public Optional<PublisherModel> findPublisherModelByName(String publisherName);
 
 }
